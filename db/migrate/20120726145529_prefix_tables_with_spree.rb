a0b8652361fb7e573c040f85372a5ac08c654057
RailsCarma <github@carmatec.com>
2014-02-19 13:23:10 +0530

    Update 20120726145529_prefix_tables_with_spree.rb

--- db/migrate/20120726145529_prefix_tables_with_spree.rb
+++ db/migrate/20120726145529_prefix_tables_with_spree.rb
@@ -1,5 +1,6 @@
 class PrefixTablesWithSpree < ActiveRecord::Migration
   def change
     rename_table :suppliers, :spree_suppliers
+    rename_table :products_suppliers, :spree_products_suppliers
   end
 end
